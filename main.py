5debbf8133009ec6efdcbde1b2afef171fe35274
roseanh <109952618+roseanh@users.noreply.github.com>
2025-08-14 21:35:57 +0200

    Lesson 2
    UPDATE:
    add happiness to attributes
    
    Instance Methods, Changing Pet State
    Add the following methods to your Pet class:
    feed() – decreases hunger
    play() – decreases energy and increases happiness
    sleep() – increases energy
    
    Modify the methods so that they update the instance's attributes accordingly.
    Test calling these methods on an instance of the class.

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 class Pet:
-    def __init__(self, name, hunger, energy):
+    def __init__(self, name, hunger, energy, happiness):
         self.name = name
         self.hunger = max(0,min(100,hunger))
         self.energy = max(0,min(100,energy))
+        self.happiness = max(0,min(100,happiness))
 
-my_pet = Pet("Joseph", 20 , 15)
+    def feed(self):
+        self.hunger = max(0, self.hunger -10)
+    
+    def play (self): 
+        self.energy = max(0, self.energy - 10)
+        self.happiness = max(0,self.happiness + 10)
+    
+    def sleep(self):
+        self.energy = max(0, self.energy + 10)
 
+
+my_pet = Pet("Joseph", 20 , 15, 10)
+my_pet.feed()
+my_pet.play()
+my_pet.sleep()
+
+    
 print(my_pet.name)
-print(my_pet.hunger)
-print(my_pet.energy)
+ #print(my_pet.hunger)
+ #print(my_pet.energy)
+print(my_pet.happiness)
+
+
