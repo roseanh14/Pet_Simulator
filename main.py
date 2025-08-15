08022c93256a683fb150eb8d687e9d9c47293a6f
roseanh <109952618+roseanh@users.noreply.github.com>
2025-08-15 13:40:56 +0200

    UPDATE-02:
    Add an attribute named abilities or instincts, which should be a list type, as an animal can have multiple instincts.
    
    Add a __str__() method for a nicely formatted display of the pet's information.
    Set default values for the attributes in the __init__ method if they are not provided.
    
    Create an example where hunger and energy are not given, and the default values are used.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 class Pet:
-    def __init__(self, name, hunger, energy, happiness):
+    def __init__(self, name, hunger= 50 , energy = 50  ,happiness = 15 , instincts = None):
         self.name = name
         self.hunger = max(0,min(100,hunger))
         self.energy = max(0,min(100,energy))
         self.happiness = max(0,min(100,happiness))
+        self.instincts = list(instincts) if instincts is not None else []
 
     def feed(self):
         self.hunger = max(0, self.hunger -10)
@@ -15,8 +16,15 @@ class Pet:
     def sleep(self):
         self.energy = max(0, self.energy + 10)
 
+    def __str__(self):
+        instincts_formatted = ", ".join(self.instincts)
+        return (f"Pet : {self.name}\n"
+               f"Hunger: {self.hunger}\n"
+               f"Energy: {self.energy}\n"
+               f"Happiness: {self.happiness}\n"
+               f"Instincts: {instincts_formatted}")
 
-my_pet = Pet("Joseph", 20 , 15, 10)
+my_pet = Pet("Joseph", 20 , 15, 10, ["hunt", "climb", "purr"])
 my_pet.feed()
 my_pet.play()
 my_pet.sleep()
@@ -26,5 +34,11 @@ print(my_pet.name)
  #print(my_pet.hunger)
  #print(my_pet.energy)
 print(my_pet.happiness)
+print(my_pet.instincts)
 
 
+pet_default = Pet("Mia",hunger = 50, energy = 50, happiness=20 ,instincts=["purr", "climb"])
+print(pet_default)
+
+pet_default = Pet("Mai", happiness=20, instincts=["meow", "sip"])
+print(pet_default)
