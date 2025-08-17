81f94fd1a39af709096487ca717317ab18b2bd7c
roseanh <109952618+roseanh@users.noreply.github.com>
2025-08-17 14:58:21 +0200

    Polymorphism and isinstance()
    Create a list of pets from different classes.
    Write a function that calls make_sound() on each one.
    Use isinstance() to distinguish specific actions (e.g., a parrot can repeat words).

--- project/main.py
+++ project/main.py
@@ -3,38 +3,52 @@ from dog import Dog
 from cat import Cat
 from parrot import Parrot
 
-#my_pet = Pet("Joseph", 20 , 15, 10, ["hunt", "climb", "purr"])
-#my_pet.feed()
-#my_pet.play()
-#my_pet.sleep()
-
-    
-#print(my_pet.name)
-#print(my_pet.hunger)
-#print(my_pet.energy)
-#print(my_pet.happiness)
-#print(my_pet.instincts)
+# my_pet = Pet("Joseph", 20 , 15, 10, ["hunt", "climb", "purr"])
+# my_pet.feed()
+# my_pet.play()
+# my_pet.sleep()
 
 
-#pet_default = Pet("Mia",hunger = 50, energy = 50, happiness=20 ,instincts=["purr", "climb"])
-#print(pet_default)
+# print(my_pet.name)
+# print(my_pet.hunger)
+# print(my_pet.energy)
+# print(my_pet.happiness)
+# print(my_pet.instincts)
 
-#pet_default = Pet("Mai", happiness=20, instincts=["meow", "sip"])
-#rint(pet_default)
 
-#my_pet = Pet("Mia")
+# pet_default = Pet("Mia",hunger = 50, energy = 50, happiness=20 ,instincts=["purr", "climb"])
+# print(pet_default)
 
-#my_pet.hunger = 5
-#print(my_pet.hunger)
+# pet_default = Pet("Mai", happiness=20, instincts=["meow", "sip"])
+# rint(pet_default)
 
-#my_pet.hunger = 150
-#print(my_pet.hunger)
+# my_pet = Pet("Mia")
 
-my_dog = Dog("Rex")
-my_dog.make_sound()
+# my_pet.hunger = 5
+# print(my_pet.hunger)
 
-my_cat = Cat("Lily")
-my_cat.make_sound()
+# my_pet.hunger = 150
+# print(my_pet.hunger)
 
-my_parrot = Parrot("Honza")
-my_parrot.make_sound()
+pets = [Dog("REX"), Cat("MIA"), Parrot("MAI")]
+
+
+def interact_with_pets(pets):
+    for x in pets:
+        x.make_sound()
+        if isinstance(x, Parrot):
+            x.repeat_word("Polly wants a cracker!")
+
+
+interact_with_pets(pets)
+
+
+# my_dog = Dog("REX")
+# my_dog.make_sound()
+
+
+# my_cat = Cat("MIA")
+# my_cat.make_sound()
+
+# my_parrot = Parrot("MAI")
+# my_parrot.make_sound()
--- project/parrot.py
+++ project/parrot.py
@@ -1,8 +1,12 @@
 from pet import Pet
 
+
 class Parrot(Pet):
     def __init__(self, name, hunger=50, energy=50, happiness=15, instincts=None):
         super().__init__(name, hunger, energy, happiness, instincts)
-    
+
     def make_sound(self):
         print("Hello!")
+
+    def repeat_word(self, word):
+        print(word)
